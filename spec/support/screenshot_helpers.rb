47637c00dd66821a664dc82f55a8fe12778d45c2
Carlos Rodriguez <ucroxlive@gmail.com>
2025-12-09 17:09:27 -0500

    refactor ci 6

--- spec/support/screenshot_helpers.rb
+++ spec/support/screenshot_helpers.rb
@@ -1,11 +1,7 @@
 RSpec.configure do |config|
   config.after(:each, type: :system) do |example|
-    next if example.exception.blank?
-
-    if ENV['CI'].present?
-      save_screenshot("tmp/capybara/#{example.full_description.parameterize}.png")
-      puts "\n[CI] Screenshot saved for failed example: #{example.full_description}"
-    else
+    if example.exception.present?
+      return if ENV["CI"]
       save_and_open_screenshot
     end
   end
