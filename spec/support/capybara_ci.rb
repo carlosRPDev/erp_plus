7c84dcbe51efa2951545fd531433bb40e11a4962
Carlos Rodriguez <ucroxlive@gmail.com>
2025-12-09 16:06:10 -0500

    refactor screenshot capybara

--- spec/support/capybara_ci.rb
+++ spec/support/capybara_ci.rb
@@ -1,25 +1,28 @@
 # frozen_string_literal: true
 
-if ENV["CI"] == "true"
-  require "selenium/webdriver"
+if ENV["CI"]
+  module Capybara
+    class << self
+      def save_and_open_page(_path)
+        # no-op en CI
+      end
 
-  Capybara.register_driver :chrome_headless_ci do |app|
-    options = Selenium::WebDriver::Chrome::Options.new
+      def save_and_open_screenshot(_path)
+        # no-op en CI
+      end
+    end
+  end
 
+  Capybara.register_driver :selenium_chrome_headless_ci do |app|
+    options = ::Selenium::WebDriver::Chrome::Options.new
     options.add_argument("--headless=new")
     options.add_argument("--disable-gpu")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
-    options.add_argument("--window-size=1920,1080")
+    options.add_argument("--window-size=1400,1400")
 
-    Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
+    Capybara::Selenium::Driver.new(app, browser: :chrome, options:)
   end
-
-  Capybara.javascript_driver = :chrome_headless_ci
-
-  RSpec.configure do |config|
-    config.before(:each, type: :system) do
-      driven_by :chrome_headless_ci
-    end
-  end
+  Capybara.javascript_driver = :selenium_chrome_headless_ci
+  Capybara.default_driver    = :selenium_chrome_headless_ci
 end
