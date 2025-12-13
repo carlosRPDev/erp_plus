1a56d8579655dea15bb7b12c969607cb87b6e3d9
Carlos Rodriguez <ucroxlive@gmail.com>
2025-12-13 03:19:28 -0500

    refactor specs

--- spec/support/devise_helpers.rb
+++ spec/support/devise_helpers.rb
@@ -10,6 +10,16 @@ module DeviseRequestSpecHelpers
   def sign_out
     delete erp_core.destroy_user_session_path
   end
+
+  def sign_in_admin(admin)
+    post erp_core.admin_session_path, params: {
+      user: { email: admin.email, password: admin.password }
+    }
+  end
+
+  def sign_out_admin
+    delete erp_core.destroy_admin_session_path
+  end
 end
 
 RSpec.configure do |config|
