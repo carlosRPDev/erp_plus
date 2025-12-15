dd1a3654e5840deeeb1bebc0a1bffe3345a8c946
Carlos Rodriguez <ucroxlive@gmail.com>
2025-12-14 21:23:40 -0500

    remove file simplecov

--- spec/coverage_helper.rb
+++ spec/coverage_helper.rb
@@ -31,7 +31,7 @@ SimpleCov.start 'rails' do
   add_group 'ERP Workers', 'engines/erp_workers/app'
 
   minimum_coverage 90
-  minimum_coverage_by_file 80
+  minimum_coverage_by_file 70
 end
 
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
